bb9bb68817af172f5f40a184ede4e5015db5b9c9
samuelpolagani <samuel.polagani.learner@gmail.com>
2023-02-21 15:25:55 +0000

    dd

--- vars/sendSlackNotification.groovy
+++ vars/sendSlackNotification.groovy
@@ -8,7 +8,7 @@ def call(String buildStatus = 'STARTED') {
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
 
-  // Override default values based on build status
+  // Override default values based on build status    
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
